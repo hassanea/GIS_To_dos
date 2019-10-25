d2bea0ee88516ec6c6799f46c4aabb1c0a7f752a
Evan H <eahassan@oakland.edu>
2019-10-24 20:44:58 -0400

    Created map component, integratedGoogle Maps, and deleted Nav component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 import Appbar from './Appbar.js';
 import Search from './Search.js';
-import Header from './Header.js';
+//import Header from './Header.js';
 //import Buttonbar from './Buttonbar.js';
+import Map from './Map.js';
 import Footer from './Footer.js';
 
 function App() {
@@ -19,14 +20,19 @@ function App() {
       /* Main content */
       }
       <main id="mainContent" tabIndex="-1">
-       <div className="w3-container">
+      <div className="w3-row">
+       <div className="w3-container w3-col">
       {
       /* Header component */
       }
-      <Header/>
+      {
+//      <Header/>
+      }
+      <Map/>
       {
 //      <Buttonbar/>
       }
+      </div>
       </div>
       </main>
       {
---
+++ src/Map.js
@@ -0,0 +1,103 @@
+import React from 'react';
+
+
+
+const urlMapsAPI = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBm3mUKF6xpj3HzgFFALZZet1FW7ZOdqKs&callback=initMap';
+
+
+class Map extends React.Component {
+    
+    
+    
+componentDidMount(){
+    this.renderMap()
+    
+    // Error handling: For when the Google Maps API key authenication fails.
+    window.gm_authFailure = () => { 
+      alert(
+        "Google Maps error! Check out the JS Console for additional details!");
+    }
+}
+    
+/* renderMap function: Loads script with the scriptLoad function as well as it calls initMap. */
+    renderMap = () => {
+        scriptLoad(urlMapsAPI)
+        window.initMap = this.initMap
+    }
+    
+    
+        /* initMap function: Initializes the Map including markers, infowindows, and other particular details. */
+    initMap = () => {
+        // eslint-disable-next-line
+        let map = new window.google.maps.Map(document.getElementById('map'), {
+          // Sets Map's center.    
+          center: {lat: 42.6389216, lng: -83.2910468},
+          // Sets Map's zoom level.    
+          zoom: 8,   
+          draggable: true   
+        })
+        
+        
+                // Infowindow initialization
+        let infowindow = new window.google.maps.InfoWindow({maxWidth: 200})
+             
+            // Marker's initialization 
+            let marker = new window.google.maps.Marker({
+    position: {lat: 42.63829635, lng: -83.30672549},
+    map: map,
+    animation: window.google.maps.Animation.DROP,            
+    title: "Reroot Pontiac"            
+  })
+  
+         
+// Initializes Infowindow's content.          
+let contentString = `<div> ${marker.title} </div>`            
+            
+// Marker's click event.            
+marker.addListener('click', function() {
+
+        // Change the content
+        infowindow.setContent(contentString)
+
+        // Open An InfoWindow
+        infowindow.open(map, marker)
+      })    // Populates the marker's state with the markers.
+
+                     
+
+        
+        }
+    
+    
+  render() {
+      
+    return (
+        
+    <div className="map-Container" role="application" aria-label="Interactive Map" tabIndex="0">
+        <div id="map"></div> 
+    </div> 
+        
+    );
+  }
+}
+
+
+
+/* 
+
+scriptLoad function: Loads the Google Maps API script asychronously 
+
+References: https://www.klaasnotfound.com/2016/11/06/making-google-maps-work-with-react/
+    https://www.youtube.com/channel/UCcWSbBe_s-T_gZRnqFbtyIA
+*/
+const scriptLoad = (url) => {
+        let index = window.document.getElementsByTagName('script')[0]
+        let script = window.document.createElement('script')
+        script.src = url
+        script.async = true
+        script.defer = true
+        index.parentNode.insertBefore(script, index)
+        
+    }
+
+export default Map;

67e8355afbc007153454edc7763b05856772d4ee
Evan H <eahassan@oakland.edu>
2019-10-29 20:45:23 -0400

    Made create / modify task components

---
+++ src/Create.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Create extends React.Component {
+  render() {
+      
+    return (
+        
+        <div>
+        <h1>Create Task:</h1>
+        <hr/>
+        </div>
+        
+    );
+  }
+}
+
+export default Create;
---
+++ src/Modify.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Modify extends React.Component {
+  render() {
+      
+    return (
+        
+        <div>
+        <h1>Modify Task:</h1>
+        <hr/>
+        </div>
+        
+    );
+  }
+}
+
+export default Modify;

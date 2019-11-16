e90f539cea7f4db273a6604f44e4f2831ab45377
Evan H <eahassan@oakland.edu>
2019-11-15 22:03:54 -0500

    Updated plus styled 404 error page

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import HomeIcon from '@material-ui/icons/Home';
 
 class NotFound extends React.Component {
   render() {
       
-const Page404 = ({ location }) => (
-   <div>
-      <h1>No match found for <code>{location.pathname}</code></h1>
-   </div>
-);
-      
     return (
         
-        <div>
-        {Page404}
+    <div>
+     <div className="w3-row">
+     <div className="w3-container w3-center">
+          <h1>404</h1>
+          <span>No match found for <code className="code">{this.props.location.pathname}</code></span>
+          <div>The page you are looking for was not found.</div>
+          <a href="/" title="Back to Home"> <HomeIcon fontSize="large" /> </a>
+        </div>
+        </div>
         </div>
         
     );
